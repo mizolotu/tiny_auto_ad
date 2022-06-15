50be23be5f0bd14dd4435f90eb978cd0b62afb37
mikhail <mizolotu@jyu.fi>
2022-06-15 18:12:38 +0300

    15.6.2022

--- auto_unsupervised.py
+++ auto_unsupervised.py
@@ -7,7 +7,7 @@ import autokeras as ak
 
 from preprocess_data import load_dataset, split_data
 from config import *
-
+from autokeras.utils import utils
 
 class ReshapeBlock(ak.Block):
 
@@ -21,13 +21,38 @@ class ReshapeBlock(ak.Block):
         output_node = layer(input_node)
         return output_node
 
+class DistanceBlock(ak.Block):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build(self, hp, inputs=None):
+        input_node1 = tf.nest.flatten(inputs)[0]
+        input_node2 = tf.nest.flatten(inputs)[0]
+        layer = tf.keras.layers.Dense(
+            hp.Int("num_units", min_value=input_node2.shape[1], max_value=input_node2.shape[1], step=32)
+        )
+        output_node = layer(input_node1)
+        output_node = tf.expand_dims(tf.math.reduce_mean(tf.math.square(tf.math.subtract(output_node, input_node2)), axis=-1), -1)
+        return output_node
+
+class NoWeightsRegressionHead(ak.RegressionHead):
+
+    def build(self, hp, inputs=None):
+        inputs = tf.nest.flatten(inputs)
+        utils.validate_num_inputs(inputs, 1)
+        input_node = tf.clip_by_value(inputs[0], 0, 1)
+        output_node = tf.keras.layers.Lambda(lambda x: x, name=self.name)(input_node)
+        print(output_node)
+        return output_node
+
 if __name__ == '__main__':
 
-    parser = arp.ArgumentParser(description='Autokeras supervised.')
+    parser = arp.ArgumentParser(description='Autokeras unsupervised.')
     parser.add_argument('-d', '--dataset', help='Dataset name', default='bearing', choices=['fan', 'bearing'])
     parser.add_argument('-t', '--trials', help='Number of trials', default=1, type=int)
     parser.add_argument('-g', '--gpu', help='GPU', default='-1')
-    parser.add_argument('-f', '--feature_extractors', help='Feature extractors', nargs='+', default=['raw'])
+    parser.add_argument('-f', '--feature_extractors', help='Feature extractors', nargs='+', default=['raw', 'pam'])
     args = parser.parse_args()
 
     if args.gpu is None:
@@ -43,7 +68,7 @@ if __name__ == '__main__':
 
     data_fpath = osp.join(DATA_DIR, dataset)
     target_dataset = load_dataset(data_fpath, series_len=32, series_step=4, labels=labels, feature_extractors=args.feature_extractors)
-    data = split_data(target_dataset, train_on_normal=True, shuffle_features=False)
+    data = split_data(target_dataset, train_on_normal=False, shuffle_features=False)
 
     inp_shape = data['tr'][0].shape[1:]
 
@@ -56,19 +81,21 @@ if __name__ == '__main__':
 
     print(n_tr, n_val, n_inf, inp_shape)
 
-    input_node = ak.StructuredDataInput()
-    output_node = ak.Normalization()(input_node)
+    input_node1 = ak.StructuredDataInput()
+    input_node2 = ak.StructuredDataInput()
+    output_node = ak.Normalization()(input_node1)
     if len(inp_shape) > 1:
         output_node = ReshapeBlock(shape=inp_shape)(output_node)
         output_node = ak.ConvBlock()(output_node)
     output_node = ak.DenseBlock()(output_node)
-    output_node = ak.ClassificationHead(loss="binary_crossentropy", metrics=["binary_accuracy"])(output_node)
+    output_node = DistanceBlock()([output_node, input_node2])
+    output_node = NoWeightsRegressionHead(loss="mean_squared_error", metrics=[tf.keras.metrics.AUC()])(output_node)
 
     project_name = '_'.join(['automodel', args.dataset, *[str(fe) for fe in args.feature_extractors]])
 
     clf = ak.AutoModel(
         project_name=project_name,
-        inputs=input_node,
+        inputs=[input_node1, input_node2],
         outputs=output_node,
         #objective='val_binary_accuracy',
         objective='val_loss',
@@ -76,23 +103,22 @@ if __name__ == '__main__':
         max_trials=args.trials
     )
     clf.fit(
-        x_tr, data['tr'][1],
-        validation_data=(x_val, data['val'][1]),
+        [x_tr, x_tr], data['tr'][1],
+        validation_data=([x_val, x_val], data['val'][1]),
         epochs=1000,
         batch_size=512,
         callbacks=[
             tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=100, mode='min', restore_best_weights=True)
         ],
-        verbose=1
+        verbose=2
     )
 
     model = clf.export_model()
     model.summary()
 
-    model_fpath = osp.join('model_autokeras', args.dataset, *[str(fe) for fe in args.feature_extractors])
-    model.save(model_fpath)
+    model_fpath = osp.join('unsupervised_model_autokeras', args.dataset, *[str(fe) for fe in args.feature_extractors])
+    #model.save(model_fpath)
 
-    loaded_model = tf.keras.models.load_model(model_fpath, custom_objects=ak.CUSTOM_OBJECTS)
-
-    h = clf.evaluate(x_inf, data['inf'][1])
+    #loaded_model = tf.keras.models.load_model(model_fpath, custom_objects=ak.CUSTOM_OBJECTS)
+    h = clf.evaluate([x_inf, x_inf], data['inf'][1])
     print(h)
