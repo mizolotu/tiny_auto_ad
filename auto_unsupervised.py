70d72715a9094d4227be85958124b0efdade9382
mikhail <mizolotu@jyu.fi>
2022-06-15 19:20:22 +0300

    15.6.2022

--- auto_unsupervised.py
+++ auto_unsupervised.py
@@ -70,7 +70,6 @@ class EarlyStoppingAtMaxAuc(tf.keras.callbacks.Callback):
             self.best_weights = self.model.get_weights()
         else:
             self.wait += 1
-            print(self.wait, self.patience)
             if self.wait >= self.patience:
                 self.stopped_epoch = epoch
                 self.model.stop_training = True
