391535f236fbf6f633fb6d1a04e35d587362b54e
mikhail <mizolotu@jyu.fi>
2022-06-22 20:34:00 +0300

    22.06.2022

--- svdd.py
+++ svdd.py
@@ -106,6 +106,7 @@ if __name__ == '__main__':
     thr = np.mean(p) + alpha * np.std(p)
     predictions = np.zeros(len(p))
     predictions[np.where(predictions > thr)[0]] = 1
+    print(thr)
     acc = len(np.where(predictions == data['inf'][1])[0]) / data['inf'][1].shape[0]
     fpr = len(np.where((predictions == 1) & (data['inf'][1] == 0))[0]) / (1e-10 + len(np.where(data['inf'][1] == 0)[0]))
     tpr = len(np.where((predictions == 1) & (data['inf'][1] == 1))[0]) / (1e-10 + len(np.where(data['inf'][1] == 1)[0]))
