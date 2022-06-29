f622b366d490f1f0fa459e09c4abc183ec514e99
mikhail <mizolotu@jyu.fi>
2022-06-29 11:48:18 +0300

    29.6.2022

--- preprocess_data.py
+++ preprocess_data.py
@@ -91,6 +91,10 @@ def freq1(X):
     assert len(X.shape) == 3
     return np.vstack([x[0, :] for x in X])
 
+def flat(X):
+    assert len(X.shape) == 3
+    return np.vstack([x.flatten() for x in X])
+
 def interval_fix_fft(x, step=8, m=3, n_fft_features=1, fpath='libraries/fix_fft_32k_dll/fix_fft_32k.so'):
     ff = cdll.LoadLibrary(fpath)
     ff.fix_fft.argtypes = [POINTER(c_short), POINTER(c_short), c_short, c_short]
